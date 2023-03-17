5647863625ea2103a04a192fc2dfb7a5afb8e9c9
Alexander <alekswonder@yandex.ru>
2023-03-17 15:35:48 +0500

    Add default router for models

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,25 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import CategoryViewSet, GenreViewSet, TitleViewSet
+
+router_api_v1 = DefaultRouter()
+router_api_v1.register(
+    r'titles',
+    TitleViewSet,
+    basename='titles'
+)
+router_api_v1.register(
+    r'genres',
+    GenreViewSet,
+    basename='genres'
+)
+router_api_v1.register(
+    r'categories',
+    CategoryViewSet,
+    basename='categories'
+)
+
+urlpatterns = [
+    path('v1/', include(router_api_v1.urls))
+]
