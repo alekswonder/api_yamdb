fb1cbc576cd76398acff52956ab0c5ab334b501d
Alexander <alekswonder@yandex.ru>
2023-03-17 15:15:57 +0500

    Add viewSets for base models

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,2 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
+from .serializers import TitleSerializer, GenreSerializer, CategorySerializer
+from titles.models import Title, Genre, Category
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    # TODO: permission_classes = ...
+    # TODO: pagination_class = ...
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    # TODO: permission_classes = ...
+    # TODO: pagination_class = ...
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    # TODO: permission_classes = ...
+    # TODO: pagination_class = ...
