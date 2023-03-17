6d0356b23f60697cfbd37b2f6182b44da6aedc40
Nikita Kakovka <n.kakoffka@yandex.ru>
2023-03-17 10:33:56 +0300

    custom user add

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,10 +3,33 @@ from django.db import models
 
 
 class User(AbstractUser):
-    username = models.CharField(
-        max_length=150,
-        unique=True,)
-    first_name = models.CharField('Имя', max_length=150, blank=True)
-    last_name = models.CharField('Фамилия', max_length=150, blank=True)
-    email = models.EmailField('Почта', unique=True, max_length=254)
-    bio = models.TextField('Биография', blank=True,)
+    """Кастомная модель юзера"""
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+    ROLE_CHOICES = (
+        ('user', 'user'), ('moderator', 'moderator'), ('admin', 'admin')
+    )
+    username = models.CharField(max_length=150,
+                                unique=True,)
+    first_name = models.CharField(verbose_name='Имя',
+                                  max_length=150,
+                                  blank=True)
+    last_name = models.CharField(verbose_name='Фамилия',
+                                 max_length=150,
+                                 blank=True)
+    email = models.EmailField(verbose_name='Почта',
+                              unique=True,
+                              max_length=254)
+    bio = models.TextField(verbose_name='Биография',
+                           blank=True,)
+    role = models.CharField(max_length=30,
+                            choices=ROLE_CHOICES,
+                            default='user')
+
+    def save(self, *args, **kwargs):
+        if self.role == 'admin':
+            self.is_superuser = True
+        elif self.role == 'moderator':
+            self.is_staff = True
+        super().save(*args, **kwargs)
