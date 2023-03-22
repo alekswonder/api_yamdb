7c4e7fdfb8d2fa4a7f9ed17e4f337cfcdd8d39e8
Alexander <alekswonder@yandex.ru>
2023-03-22 15:33:49 +0500

    Add working custom manager command

--- api_yamdb/api/management/commands/fulfill.py
+++ api_yamdb/api/management/commands/fulfill.py
@@ -1,4 +1,10 @@
+import csv
+
 from django.core.management.base import BaseCommand
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import IntegrityError
+
+from titles.models import *
 
 
 class Command(BaseCommand):
@@ -11,18 +17,64 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for file_name in options['file_names']:
-            with open(f'static/data/{file_name}.csv', 'r') as data_file:
-                f_name = file_name
+            with open(f'static/data/{file_name}.csv', 'r',
+                      encoding='utf-8') as data_file:
+                f_name = file_name.capitalize()
                 if f_name.endswith('s'):
-                    f_name = file_name[:len(f_name) - 1]
+                    f_name = file_name[:len(f_name) - 1].capitalize()
                 if f_name.find('_') != -1:
                     f_name = file_name.replace('_',
                                                ' ').title().replace(' ', '')
-                fields = [i for i in data_file.readline().rstrip('\n'
-                                                                 ).split(',')]
-                values = [j.split(',') for j in
-                          [i.rstrip('\n') for i in data_file.readlines()]]
-                model_name = eval(f_name)
-                bulk_list = [model_name(dict(zip(fields, val)))
-                             for val in values]
-                model_name.objects.bulk_create(bulk_list)
+                data = [
+                    i for i in csv.DictReader(
+                        data_file,
+                        delimiter=',',
+                        quotechar='"'
+                    )
+                ]
+                try:
+                    if f_name == 'Title':
+                        for kwargs in data:
+                            eval(f_name).objects.create(
+                                id=kwargs['id'],
+                                name=kwargs['name'],
+                                year=kwargs['year'],
+                                category=Category.objects.get(
+                                    pk=kwargs['category_id'])
+                            )
+                    elif f_name == 'GenreTitle':
+                        for kwargs in data:
+                            eval(f_name).objects.create(
+                                id=kwargs['id'],
+                                genre=Genre.objects.get(pk=kwargs['genre_id']),
+                                title=Title.objects.get(pk=kwargs['title_id']),
+                            )
+                    elif f_name == 'Review':
+                        for kwargs in data:
+                            eval(f_name).objects.create(
+                                id=kwargs['id'],
+                                title=Title.objects.get(pk=kwargs['title_id']),
+                                text=kwargs['text'],
+                                author=User.objects.get(pk=kwargs['author']),
+                                score=kwargs['score'],
+                                pub_date=kwargs['pub_date']
+                            )
+                    elif f_name == 'Comment':
+                        for kwargs in data:
+                            eval(f_name).objects.create(
+                                id=kwargs['id'],
+                                review=Review.objects.get(
+                                    pk=kwargs['review_id']
+                                ),
+                                text=kwargs['text'],
+                                author=User.objects.get(pk=kwargs['author']),
+                                pub_date=kwargs['pub_date']
+                            )
+                    else:
+                        bulk_list = [eval(f_name)(**kwargs) for kwargs in data]
+                        eval(f_name).objects.bulk_create(bulk_list)
+                except ObjectDoesNotExist:
+                    print(f'{f_name} with id:{kwargs["id"]} has problems with'
+                          f' its foreign key')
+                except IntegrityError:
+                    print(f'{f_name} with id:{kwargs["id"]} already exists')
