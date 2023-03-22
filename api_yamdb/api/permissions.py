bb2e31daa95de687acb80f83427790fbce9f52a5
Nikita Kakovka <n.kakoffka@yandex.ru>
2023-03-22 21:10:24 +0300

    Add UserViewSet

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import SAFE_METHODS, BasePermission
+from rest_framework.permissions import BasePermission
 
 from users.models import User
 
@@ -9,39 +9,3 @@ class CustomAdminPermission(BasePermission):
         return (request.user.is_authenticated
                 and (request.user.role == User.ADMIN
                      or request.user.is_superuser))
-
-
-class AuthorOrReadOnly(BasePermission):
-    """Допуск для автора"""
-
-    def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS
-                or obj.author == request.user)
-
-
-class AdminOrAuthorOrReadOnly(BasePermission):
-    """Допуск для автора или админа"""
-
-    def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS
-                or request.user.is_authenticated)
-
-    def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS
-                or obj.author == request.user
-                or request.user.is_superuser
-                or request.user.is_staff)
-
-
-class SafeMethodAdminPermission(BasePermission):
-    """Допуск для админа"""
-    def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS
-                or (request.user.is_authenticated
-                    and request.user.is_superuser))
-
-    def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS
-                or (request.user.is_authenticated
-                    and (request.user.role == User.ADMIN
-                         or request.user.is_superuser)))
